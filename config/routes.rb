54d2859cbc505208776f049377efd0e42fe09b26
ZakharDay <zakhar.day@gmail.com>
2018-11-06 11:02:40 +0300

    Routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   resources :comics
-    resources :frames
-    resources :speeches
-    resources :bubbles
-    resources :figures
-    resources :images
+  resources :frames
+  resources :speeches
+  resources :bubbles
+  resources :figures
+  resources :images
+
+  root 'comics#index'
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
