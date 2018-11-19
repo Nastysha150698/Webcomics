1ba8499bac5b535cc6c01ce63d1a8fdeae8441ea
Alexander Moskovskiy <aleksandrmoskovskiy@a-mosk-2.local>
2018-11-19 13:18:31 +0300

    font_appender done

--- app/models/speech.rb
+++ app/models/speech.rb
@@ -1,6 +1,7 @@
 class Speech < ApplicationRecord
   belongs_to :comic
   belongs_to :frames, optional: true
+  belongs_to :font
 
   validates :comic_id, :text, :x, :y, :width, :height, :font_family, :font_size, :font_color, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_13_120826) do
+ActiveRecord::Schema.define(version: 2018_11_19_081212) do
 
   create_table "bubbles", force: :cascade do |t|
     t.string "figure"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2018_11_13_120826) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "comic_id"
+  end
+
+  create_table "fonts", force: :cascade do |t|
+    t.string "title"
+    t.string "link"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "frames", force: :cascade do |t|
@@ -86,6 +93,7 @@ ActiveRecord::Schema.define(version: 2018_11_13_120826) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "comic_id"
+    t.integer "font_id"
   end
 
 end
