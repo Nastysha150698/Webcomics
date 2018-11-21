d9dd92eb2a93ae61f74f95eed8072878979503a2
digitalkill3rrrE <mkiller.shinoda@gmail.com>
2018-11-21 23:30:17 +0300

    seeds file intermediary changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,177 +11,108 @@ end
 @comics = Comic.all
 
 # Create Comic
-@comics = [
-  {
-    name: 'Nonstop Bar'
-  }
-]
-
-def create_comic(comic)
+def create_comic
   Comic.create(
-    name: comic[:name]
+    name: 'Nonstop Bar'
   )
 end
 
-@comics.each do |comic|
-  c = create_comic(comic)
-  puts "comic #{c.name} created"
+
+# Create Comics
+10.times do
+  name = random_name
+  c = Comic.new(name: name)
+
+  if c.save
+    puts "Comics #{name} created"
+  else
+    puts "Error. Comics #{name} not created"
+  end
+end
+
+# Create Figures
+def random_size
+  rand(1000)
+end
+
+def random_border
+  rand(30)
+end
+
+def random_color
+  ['red', 'green', 'blue', 'orange', 'black', 'yellow', 'magenta', 'cyan'].sample
+end
+
+# Fake comic data
+100.times do
+  name = random_name
+  comic = @comics.sample
+  f = comic.figures.new(
+    figure:           name,
+    x:                random_size,
+    y:                random_size,
+    width:            random_size,
+    height:           random_size,
+    border_width:     random_border,
+    border_color:     random_color,
+    background_color: random_color
+  )
+
+  if f.save
+    puts "Figure #{name} created"
+  else
+    puts "Error. Figure #{name} not created"
+  end
 end
 
 
-#Create figure {background}
-@figures = [
-  {
-    comic_id: 1,
-    x: 0,
-    y: 0,
-    width: 1300,
-    height: 850,
-    background_color: 'black'
-  }
-]
+#Create real comic
+c = Comic.create(name: 'Nonstop Bar')
 
-def create_figure(figure)
-  Figure.create(
-    comic_id: figure[:comic_id],
-    x: figure[:x],
-    y: figure[:y],
-    width: figure[:width],
-    height: figure[:height],
+def create_comic(comic, figure)
+  comic.figures.create(
+    comic_id:         figure[:comic_id],
+    figure:           figure[:figure],
+    x:                figure[:x],
+    y:                figure[:y],
+    width:            figure[:width],
+    height:           figure[:height],
     background_color: figure[:background_color]
   )
 end
 
-@figures.each do |figure|
-  f = create_figure(figure)
-  puts "figure #{f.figure} created"
-end
-
-
-
-#Create figures {old}
-# @figures = [
-#   {
-#     comic_id: 1,
-#     figure: 'big frame',
-#     x: 200,
-#     y: 120,
-#     width: 422,
-#     height: 605,
-#     background_color: 'black'
-#   }, {
-#     comic_id: 1,
-#     figure: '1 frame',
-#     x: 640,
-#     y: 120,
-#     width: 426,
-#     height: 137,
-#     background_color: 'red'
-#   }, {
-#     comic_id: 1,
-#     figure: '2 frame',
-#     x: 640,
-#     y: 277,
-#     width: 426,
-#     height: 136,
-#     background_color: 'green'
-#   }, {
-#     comic_id: 1,
-#     figure: '3 frame',
-#     x: 640,
-#     y: 432,
-#     width: 426,
-#     height: 136,
-#     background_color: 'yellow'
-#   }, {
-#     comic_id: 1,
-#     figure: '4 frame',
-#     x: 640,
-#     y: 588,
-#     width: 426,
-#     height: 137,
-#     background_color: 'blue'
-#   }
-# ]
-#
-# def create_figure(figure)
-#   Figure.create(
-#     comic_id: figure[:comic_id],
-#     figure: figure[:figure],
-#     x: figure[:x],
-#     y: figure[:y],
-#     width: figure[:width],
-#     height: figure[:height],
-#     background_color: figure[:background_color]
-#   )
-# end
-#
-# @figures.each do |figure|
-#   f = create_figure(figure)
-#   puts "figure #{f.figure} created"
-# end
-
-
-
-#Create images
-@images = [
+figures = [
   {
-    comic_id: 1,
-    # image: '../public/uploads/image/image/big_fr_img.png',
-    x: 200,
-    y: 120,
-    width: 422,
-    height: 605
-  }, {
-    comic_id: 1,
-    # image: '../public/uploads/image/image/1fr_img.png',
-    x: 640,
-    y: 120,
-    width: 426,
-    height: 137
-  }, {
-    comic_id: 1,
-    # image: '../public/uploads/image/image/2fr_img.png',
-    x: 640,
-    y: 277,
-    width: 426,
-    height: 136
-  }, {
-    comic_id: 1,
-    # image: '../public/uploads/image/image/3fr_img.png',
-    x: 640,
-    y: 432,
-    width: 426,
-    height: 136
-  }, {
-    comic_id: 1,
-    # image: 'url(../public/uploads/image/image/4fr_img.png)',
-    x: 640,
-    y: 588,
-    width: 426,
-    height: 137
+    comic_id:          1,
+    figure:           'background',
+    x:                 0,
+    y:                 0,
+    width:             1300,
+    height:            850,
+    background_color: 'black'
   }
 ]
 
-def create_image(image)
-  Image.create(
-    comic_id: image[:comic_id],
-    image: image[:image],
-    x: image[:x],
-    y: image[:y],
-    width: image[:width],
-    height: image[:height]
+figures.each do |f|
+  create_comics(c, f)
+end
+
+
+#Create frame
+def create_comic(comic, frame)
+  comic.frames.create(
+    comic_id:         frame[:comic_id],
+    frame:            frame[:frame],
+    x:                frame[:x],
+    y:                frame[:y],
+    width:            frame[:width],
+    height:           frame[:height],
+    border_width:     frame[:border_width],
+    border_color:     frame[:border_color]
   )
 end
 
-@images.each do |image|
-  i = create_image(image)
-  puts "image #{i.image} created"
-end
-
-
-#Create frames
-@frames = [
+frames = [
   {
     comic_id: 1,
     frame: 'big frame',
@@ -230,20 +161,68 @@ end
   }
 ]
 
-def create_frame(frame)
-  Frame.create(
-    comic_id: frame[:comic_id],
-    frame: frame[:frame],
-    x: frame[:x],
-    y: frame[:y],
-    width: frame[:width],
-    height: frame[:height],
-    border_width: frame[:border_width],
-    border_color: frame[:border_color]
+frames.each do |frame|
+  create_comics(c, frame)
+end
+
+
+#Create images
+def upload_image
+  uploader = ImageUploader.new(Comic.new, :image)
+  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, '../public/uploaders/image/image/1fr_img.png'))))
+  uploader
+end
+
+def create_comic(comic, image)
+  comic.image.create(
+    comic_id:         frame[:comic_id],
+    image:            frame[:upload_image],
+    x:                frame[:x],
+    y:                frame[:y],
+    width:            frame[:width],
+    height:           frame[:height]
   )
 end
 
-@frames.each do |frame|
-  fr = create_frame(frame)
-  puts "frame #{fr.frame} created"
+images = [
+  {
+    comic_id: 1,
+    image: 'big frame',
+    x: 198,
+    y: 118,
+    width: 422,
+    height: 605
+  }, {
+    comic_id: 1,
+    image: upload_image,
+    x: 638,
+    y: 118,
+    width: 426,
+    height: 137
+  }, {
+    comic_id: 1,
+    image: '2 frame',
+    x: 638,
+    y: 275,
+    width: 426,
+    height: 136
+  }, {
+    comic_id: 1,
+    image: '3 frame',
+    x: 638,
+    y: 430,
+    width: 426,
+    height: 136
+  }, {
+    comic_id: 1,
+    image: '4 frame',
+    x: 638,
+    y: 586,
+    width: 426,
+    height: 137
+  }
+]
+
+images.each do |i|
+  create_comics(c, i)
 end
