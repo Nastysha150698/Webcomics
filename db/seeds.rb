00db468185dfffd364bbf3a1159240f04f7f5844
digitalkill3rrrE <mkiller.shinoda@gmail.com>
2018-11-27 00:27:30 +0300

    images seed added to comic

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,14 +10,6 @@ end
 
 @comics = Comic.all
 
-# Create Comic
-def create_comic
-  Comic.create(
-    name: 'Nonstop Bar'
-  )
-end
-
-
 # Create Comics
 10.times do
   name = random_name
@@ -30,7 +22,7 @@ end
   end
 end
 
-# Create Figures
+# Create random Figures
 def random_size
   rand(1000)
 end
@@ -41,6 +33,67 @@ end
 
 def random_color
   ['red', 'green', 'blue', 'orange', 'black', 'yellow', 'magenta', 'cyan'].sample
+end
+
+
+# # Create random Text
+# def random_text
+#   ['yo, rap', 'wow!', 'kawabunga', 'she might be little upset about this...', 'Эта бабушка что-то подозревает', 'knock-knock'].sample
+# end
+#
+# def random_font_family
+#   ['Roboto', 'Montserrat', 'Oswald', 'Merriweather', 'Playfair Display', 'Rubik', 'Amatic SC', 'Pacifico', 'Old Standard TT', 'Press Start 2P'].sample
+# end
+#
+# def random_font_size
+#   rand(60)
+# end
+#
+# 30.times do
+#   name = random_name
+#   comic = @comics.sample
+#   s = comic.speeches.new(
+#     text: random_text,
+#     x:                random_size,
+#     y:                random_size,
+#     width:            random_size,
+#     height:           random_size,
+#     font_family:      random_font_family,
+#     font_size:        random_font_size,
+#     font_color:       random_color
+#   )
+#
+#   if s.save
+#     puts "Text #{name} created"
+#   else
+#     puts "Text #{name} not created"
+#   end
+# end
+
+
+# Create random Images
+def upload_fake_image
+  uploader = ImageUploader.new(Image.new, :image)
+  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, 'lib/tasks/images', '*')).sample))
+  uploader
+end
+
+50.times do
+  name = random_name
+  comic = @comics.sample
+  i = comic.images.new(
+    image:            upload_fake_image,
+    x:                random_size,
+    y:                random_size,
+    width:            random_size,
+    height:           random_size
+  )
+
+  if i.save
+    puts "Image #{name} created"
+  else
+    puts "Error. Image #{name} not created"
+  end
 end
 
 # Fake comic data
@@ -66,7 +119,7 @@ end
 end
 
 
-#Create real comic
+# Create real Comic
 c = Comic.create(name: 'Nonstop Bar')
 
 def create_comic(comic, figure)
@@ -94,135 +147,135 @@ figures = [
 ]
 
 figures.each do |f|
-  create_comics(c, f)
+  create_comic(c, f)
 end
 
-
-#Create frame
-def create_comic(comic, frame)
-  comic.frames.create(
-    comic_id:         frame[:comic_id],
-    frame:            frame[:frame],
-    x:                frame[:x],
-    y:                frame[:y],
-    width:            frame[:width],
-    height:           frame[:height],
-    border_width:     frame[:border_width],
-    border_color:     frame[:border_color]
-  )
-end
-
-frames = [
-  {
-    comic_id: 1,
-    frame: 'big frame',
-    x: 198,
-    y: 118,
-    width: 422,
-    height: 605,
-    border_width: 2,
-    border_color: 'white'
-  }, {
-    comic_id: 1,
-    frame: '1 frame',
-    x: 638,
-    y: 118,
-    width: 426,
-    height: 137,
-    border_width: 2,
-    border_color: 'white'
-  }, {
-    comic_id: 1,
-    frame: '2 frame',
-    x: 638,
-    y: 275,
-    width: 426,
-    height: 136,
-    border_width: 2,
-    border_color: 'white'
-  }, {
-    comic_id: 1,
-    frame: '3 frame',
-    x: 638,
-    y: 430,
-    width: 426,
-    height: 136,
-    border_width: 2,
-    border_color: 'white'
-  }, {
-    comic_id: 1,
-    frame: '4 frame',
-    x: 638,
-    y: 586,
-    width: 426,
-    height: 137,
-    border_width: 2,
-    border_color: 'white'
-  }
-]
-
-frames.each do |frame|
-  create_comics(c, frame)
-end
-
-
-#Create images
-def upload_image
-  uploader = ImageUploader.new(Comic.new, :image)
-  uploader.cache!(File.open(Dir.glob(File.join(Rails.root, '../public/uploaders/image/image/1fr_img.png'))))
+# Create Image uploader
+def upload_image(image)
+  uploader = ImageUploader.new(Image.new, :image)
+  uploader.cache!(File.open(File.join(Rails.root, 'lib/tasks/images', image )))
   uploader
 end
 
+
+# Create Images
 def create_comic(comic, image)
-  comic.image.create(
-    comic_id:         frame[:comic_id],
-    image:            frame[:upload_image],
-    x:                frame[:x],
-    y:                frame[:y],
-    width:            frame[:width],
-    height:           frame[:height]
+  comic.images.create(
+    comic_id:         image[:comic_id],
+    image:            image[:image],
+    x:                image[:x],
+    y:                image[:y],
+    width:            image[:width],
+    height:           image[:height]
   )
 end
 
 images = [
   {
     comic_id: 1,
-    image: 'big frame',
-    x: 198,
-    y: 118,
-    width: 422,
+    image: upload_image("big_fr.png"),
+    x: 208,
+    y: 120,
+    width: 414,
     height: 605
   }, {
     comic_id: 1,
-    image: upload_image,
-    x: 638,
-    y: 118,
+    image: upload_image("1_fr.png"),
+    x: 640,
+    y: 120,
     width: 426,
     height: 137
   }, {
     comic_id: 1,
-    image: '2 frame',
-    x: 638,
-    y: 275,
+    image: upload_image("2_fr.png"),
+    x: 640,
+    y: 277,
     width: 426,
     height: 136
   }, {
     comic_id: 1,
-    image: '3 frame',
-    x: 638,
-    y: 430,
+    image: upload_image("3_fr.png"),
+    x: 640,
+    y: 432,
     width: 426,
     height: 136
   }, {
     comic_id: 1,
-    image: '4 frame',
-    x: 638,
-    y: 586,
+    image: upload_image("4_fr.png"),
+    x: 640,
+    y: 588,
     width: 426,
     height: 137
   }
 ]
 
 images.each do |i|
-  create_comics(c, i)
+  create_comic(c, i)
 end
+
+# Create Frames
+# def create_comic(comic, frame)
+#   comic.frames.create(
+#     comic_id:         frame[:comic_id],
+#     frame:            frame[:frame],
+#     x:                frame[:x],
+#     y:                frame[:y],
+#     width:            frame[:width],
+#     height:           frame[:height],
+#     border_width:     frame[:border_width],
+#     border_color:     frame[:border_color]
+#   )
+# end
+#
+# frames = [
+#   {
+#     comic_id:       1,
+#     frame:         'big frame',
+#     x:              198,
+#     y:              118,
+#     width:          422,
+#     height:         605,
+#     border_width:   2,
+#     border_color:  'white'
+#   }, {
+#     comic_id:       1,
+#     frame:         '1 frame',
+#     x:              638,
+#     y:              118,
+#     width:          426,
+#     height:         137,
+#     border_width:   2,
+#     border_color:  'white'
+#   }, {
+#     comic_id:       1,
+#     frame:         '2 frame',
+#     x:              638,
+#     y:              275,
+#     width:          426,
+#     height:         136,
+#     border_width:   2,
+#     border_color:  'white'
+#   }, {
+#     comic_id:       1,
+#     frame:         '3 frame',
+#     x:              638,
+#     y:              430,
+#     width:          426,
+#     height:         136,
+#     border_width:   2,
+#     border_color:  'white'
+#   }, {
+#     comic_id:       1,
+#     frame:         '4 frame',
+#     x:              638,
+#     y:              586,
+#     width:          426,
+#     height:         137,
+#     border_width:   2,
+#     border_color:  'white'
+#   }
+# ]
+#
+# frames.each do |frame|
+#   create_comic(c, frame)
+# end
